2a2854409c37794293943f876ca82a7319aff893
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-12-25 19:50:31 +0000

    Add basic project skeleton (#6)

---
+++ include/Ishiko/Time/Date.hpp
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/time/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_TIME_DATE_HPP_
+#define _ISHIKO_TIME_DATE_HPP_
+
+#endif
---
+++ src/Date.cpp
@@ -0,0 +1,7 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/time/blob/main/LICENSE.txt
+*/
+
+#include "Date.hpp"
