9e9891c7080a91e2c05ac2219db61e3bbff3b75d
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-12-25 19:27:41 +0000

    Add test file

---
+++ tests/src/main.cpp
@@ -0,0 +1,16 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/time/blob/main/LICENSE.txt
+*/
+
+#include <Ishiko/Tests/Core.h>
+
+using namespace Ishiko::Tests;
+
+int main(int argc, char* argv[])
+{
+    TestHarness theTestHarness("IshikoTime");
+
+    return theTestHarness.run();
+}
