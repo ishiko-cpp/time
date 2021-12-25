78cda720e4c4f6fbfabb740004e82e267a2280e3
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2021-12-25 18:45:57 +0000

    Add files

---
+++ include/Ishiko/Time.hpp
@@ -0,0 +1,14 @@
+/*
+    Copyright (c) 2021 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/time/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_TIME_HPP_
+#define _ISHIKO_CPP_TIME_HPP_
+
+#include "Time/Date.hpp"
+
+#include "Time/linkoptions.h"
+
+#endif
