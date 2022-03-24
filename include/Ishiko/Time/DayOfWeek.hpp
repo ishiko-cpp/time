9ee6a138b10a699b93d91dba442eee33fd1ccc5d
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-03-24 17:36:18 +0000

    Add files

---
+++ include/Ishiko/Time/DayOfWeek.hpp
@@ -0,0 +1,10 @@
+/*
+    Copyright (c) 2022 Xavier Leclercq
+    Released under the MIT License
+    See https://github.com/ishiko-cpp/time/blob/main/LICENSE.txt
+*/
+
+#ifndef _ISHIKO_CPP_TIME_DAYOFWEEK_HPP_
+#define _ISHIKO_CPP_TIME_DAYOFWEEK_HPP_
+
+#endif
