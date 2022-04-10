f5d489fe0d871421c5a5d5a3bce8a9adbd4f8f2e
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-04-10 16:07:29 +0100

    Fix test code build

--- tests/src/UTCTimeTests.hpp
+++ tests/src/UTCTimeTests.hpp
@@ -7,7 +7,7 @@
 #ifndef _ISHIKO_CPP_TIME_TESTS_UTCTIMETESTS_HPP_
 #define _ISHIKO_CPP_TIME_TESTS_UTCTIMETESTS_HPP_
 
-#include <Ishiko/Tests/Core.hpp>
+#include <Ishiko/TestFramework/Core.hpp>
 
 class UTCTimeTests : public Ishiko::TestSequence
 {
