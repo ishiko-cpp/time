0ea329dd4bc7fb85992e51c8fa497fdbd0b8628a
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2022-07-16 18:19:53 +0100

    Refactor tests (#26)

--- tests/src/main.cpp
+++ tests/src/main.cpp
@@ -10,18 +10,38 @@
 #include "UTCTimeTests.hpp"
 #include <Ishiko/TestFramework/Core.hpp>
 #include <Ishiko/Time/linkoptions.hpp>
+#include <exception>
 
 using namespace Ishiko;
 
 int main(int argc, char* argv[])
 {
-    TestHarness theTestHarness("IshikoTime");
+    try
+    {
+        TestHarness::CommandLineSpecification commandLineSpec;
+        commandLineSpec.setDefaultValue("context.data", "../../data");
+        commandLineSpec.setDefaultValue("context.output", "../../output");
+        commandLineSpec.setDefaultValue("context.reference", "../../reference");
 
-    TestSequence& theTests = theTestHarness.tests();
-    theTests.append<SystemTimeTests>();
-    theTests.append<DateTests>();
-    theTests.append<UTCTimeTests>();
-    theTests.append<StopwatchTests>();
+        Configuration configuration = commandLineSpec.createDefaultConfiguration();
+        CommandLineParser::parse(commandLineSpec, argc, argv, configuration);
 
-    return theTestHarness.run();
+        TestHarness theTestHarness("Ishiko/C++ Time Library Tests", configuration);
+
+        TestSequence& theTests = theTestHarness.tests();
+        theTests.append<SystemTimeTests>();
+        theTests.append<DateTests>();
+        theTests.append<UTCTimeTests>();
+        theTests.append<StopwatchTests>();
+
+        return theTestHarness.run();
+    }
+    catch (const std::exception& e)
+    {
+        return TestApplicationReturnCode::exception;
+    }
+    catch (...)
+    {
+        return TestApplicationReturnCode::exception;
+    }
 }
